d7c3e166c7efa7304d11f19781b339e5e4a6dba8
Suvineet Singh <suvineet1998@gmail.com>
2020-03-31 20:50:04 +0100

    [cwk2] Fixup code for Q2

--- cwk/cwk2/FindLaunchAngles.m
+++ cwk/cwk2/FindLaunchAngles.m
@@ -2,7 +2,6 @@ function [ang1, ang2] = FindLaunchAngles(target)
 % FindLaunchAngles Calculate the two launch angles to hit given target
 
 distances = @(angle) -Distance(angle) - target;
-ang1 = rad2deg(fzero(distances, pi/8));
-ang2 = rad2deg(fzero(distances, pi/4));
-
+ang1 = fzero(distances, [eps, 0.77842]);
+ang2 = fzero(distances, [0.77842, pi/2]);
 end
