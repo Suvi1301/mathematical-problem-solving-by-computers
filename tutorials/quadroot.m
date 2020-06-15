aee21e4644801861f58d6981e8bea2706bb8e4ed
Suvineet Singh <suvineet1998@gmail.com>
2020-06-15 16:46:51 +0100

    Rearrange directories and add PDF reports.

--- tutorials/quadroot.m
+++
@@ -1,8 +0,0 @@
-function y=quadroot(a)
-%QUADROOT    Roots of the quadratic function x^2-a*x+1=0
-%   quadroot(a) returns the two roots of the quadratic function x^2-a*x+1=0
-%   such that both roots are accurate even if the absolute value of a is large
-sgn = a / abs(a);
-y = [2/(a + sgn*sqrt(a^2 - 4)), (a + sgn*sqrt(a^2 - 4)/2)];
-end
-
